3e1e8c8d2b4d816b1164aeec7c0c9913686e6488
Kate Sheina Ceballos <kate.ceballos@mlhuillier.com>
2024-10-03 10:12:15 +0800

    First Commit

--- hello.php
+++ hello.php
@@ -4,15 +4,15 @@
     <title>My PHP Page</title>
     <style>
         body {
-            background-color: green;
+            background-color: red;
             color: white; /* Adding white text color for better readability */
         }
     </style>
 </head>
 <body>
     <?php
-    echo "Hello, World!";
-    echo "version 7";
+    echo "Hello, World! ";
+    echo "This is a testing DevOps Project. - Kate ";
     ?>
 </body>
 </html>
